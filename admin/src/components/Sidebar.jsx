102c71f9651b3b87830520102ba782e45808202d
Shivanand Kumar <itzshivanand@gmail.com>
2025-11-08 21:27:03 +0530

    changes in admin frontend

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = () => {
     return (
         <div className='w-[19%] min-h-screen border-r'>
             <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
-                <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-lg" to="/add" >
+                <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-lg" to="/" >
                     <img className='w-5 h-5' src={assets.add_icon} alt="" />
                     <p className='hidden md:block'>Add Items</p>
                 </NavLink>
